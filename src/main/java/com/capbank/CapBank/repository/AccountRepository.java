89ae64a77578adc631013d3d5c727cf7c7fe51bf
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-07-17 15:46:05 -0500

    builds account repository

---
+++ src/main/java/com/capbank/CapBank/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.capbank.CapBank.repository;
+
+import com.capbank.CapBank.models.Account;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends MongoRepository<Account, Long> {
+
+
+}
