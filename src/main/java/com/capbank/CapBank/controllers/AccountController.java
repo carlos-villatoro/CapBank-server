e87c3c97863962362c5f2431812d4af256a4aec0
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-07-17 15:54:12 -0500

    builds account controller

---
+++ src/main/java/com/capbank/CapBank/controllers/AccountController.java
@@ -0,0 +1,28 @@
+package com.capbank.CapBank.controllers;
+
+
+import com.capbank.CapBank.models.Account;
+import com.capbank.CapBank.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @Autowired
+    public AccountRepository accountRepository;
+
+    @GetMapping("/")
+    public List<Account> getAllAccounts(){
+        return accountRepository.findAll();
+    }
+
+    @PostMapping("/create")
+    public String createAccount(@RequestBody Account account){
+        Account insertedAccount = accountRepository.insert(account);
+        return "Account created" + insertedAccount.getAccountNumber();
+    }
+}
